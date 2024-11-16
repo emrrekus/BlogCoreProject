7d339921e808051c57491ce2009fd369868d4092
Emre <112270755+emrrekus@users.noreply.github.com>
2024-11-17 02:31:49 +0300

    Add project files.

---
+++ BlogCoreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogCoreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
