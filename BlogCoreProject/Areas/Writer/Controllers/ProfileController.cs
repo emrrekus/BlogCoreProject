53a070336e69314a4586e2a449d2d39c67f36e8b
Emre <112270755+emrrekus@users.noreply.github.com>
2024-11-19 16:48:01 +0300

    ProfileEklendi.

---
+++ BlogCoreProject/Areas/Writer/Controllers/ProfileController.cs
@@ -0,0 +1,44 @@
+﻿using BlogCoreProject.Areas.Writer.Models;
+using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCoreProject.Areas.Writer.Controllers
+{
+
+	[AllowAnonymous]
+	[Area("Writer")]
+	[Route("Writer/[controller]/[action]")]
+	public class ProfileController : Controller
+	{
+		private readonly IWriterService _writerService;
+
+		public ProfileController(IWriterService writerService)
+		{
+			_writerService = writerService;
+		}
+
+		public IActionResult Index()
+		{
+			var values = _writerService.TGetById(3);
+			UserEditViewModel viewModel = new UserEditViewModel();
+			viewModel.Name = values.Name;
+			viewModel.Surname = values.Surname;
+			viewModel.Mail=values.Mail;
+			return View(viewModel);
+		}
+
+		[HttpPost]
+        public IActionResult Index(UserEditViewModel user)
+        {
+            var values = _writerService.TGetById(3);
+
+			values.Name = user.Name;
+			values.Surname=user.Surname;
+			values.Mail=user.Mail;
+
+			_writerService.TUpdate(values);
+            return RedirectToAction("Index","Default");
+        }
+    }
+}
---
+++ BlogCoreProject/Areas/Writer/Models/UserEditViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogCoreProject.Areas.Writer.Models
+{
+    public class UserEditViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Mail { get; set; }
+    }
+}
