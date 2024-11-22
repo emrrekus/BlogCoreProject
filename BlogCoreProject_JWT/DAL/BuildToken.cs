48a75975b380a3701852f8cc578330c993664f11
Emre <112270755+emrrekus@users.noreply.github.com>
2024-11-22 16:25:37 +0300

    proje tamamlandı

--- BlogCoreProject_JWT/DAL/BuildToken.cs
+++ BlogCoreProject_JWT/DAL/BuildToken.cs
@@ -9,11 +9,30 @@ namespace BlogCoreProject_JWT.DAL
 
         public string CreateToken()
         {
-            var bytes = Encoding.UTF8.GetBytes("aspnetcoreproje");
+            var keyString = "aspnetcoreproje256bitsecurekey123!";
+            var bytes = Encoding.UTF8.GetBytes(keyString);
+
             SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
-            SigningCredentials credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
-            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddMinutes(1), signingCredentials: credentials);
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: "http://localhost",
+                audience: "http://localhost",
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: credentials);
+
             JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            TokenValidationParameters validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = false,
+                ValidateIssuerSigningKey = false,
+                RequireSignedTokens = false
+            };
+
             return handler.WriteToken(token);
         }
 
